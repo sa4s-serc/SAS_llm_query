911b222bae2544f5ac79de4e7b5f1d66a7fc88f2
bassamadnan <bassamadnan2012@gmail.com>
2024-12-08 07:06:44 +0530

    restore openai llm

--- dynamic/query_refiner.py
+++ dynamic/query_refiner.py
@@ -9,7 +9,7 @@ from langchain.prompts import (
 from langchain.chains import LLMChain
 from langchain.output_parsers import StructuredOutputParser, ResponseSchema
 import json
-from codeqwen import CodeQwenLLM
+# from codeqwen import CodeQwenLLM
 
 load_dotenv()
 API_KEY = os.getenv("OPEN_AI_API_KEY")
@@ -18,8 +18,8 @@ MODEL = os.getenv("OPEN_AI_MODEL")
 class QueryRefiner:
     def __init__(self, service_manager):
         self.service_manager = service_manager
-        # self.llm = ChatOpenAI(model_name=MODEL, temperature=0.7)
-        self.llm = CodeQwenLLM()
+        self.llm = ChatOpenAI(model_name=MODEL, temperature=0.7)
+        # self.llm = CodeQwenLLM()
 
     def refine(self, query: str):
         (
